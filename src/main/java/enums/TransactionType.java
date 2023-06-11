09138d27973a6f2b9b19dad5abe788a18bbf69df
Huzeyfe Saribas <huzeyfeorhan77@gmail.com>
2023-06-12 00:18:54 +0300

    commit-4

--- src/main/java/enums/TransactionType.java
+++ src/main/java/enums/TransactionType.java
@@ -1,21 +1,20 @@
 package enums;
 
 public enum TransactionType {
-	
-	DEPOSIT(5),
-	WITHDRAW(10),
-	TRANSFER(15),
-	PAYMENT(20),
-	OTHER(100);
-	
-	private final int transactionCode;
-	
-	public int getTransactionCode() {
-		return transactionCode;
-	}
 
-	private TransactionType(int code) {
-		this.transactionCode=code;
-	}
+    DEPOSIT(5),
+    WITHDRAW(10),
+    TRANSFER(15),
+    PAYMENT(20),
+    OTHER(100);
 
+    private final int transactionCode;
+
+    public int getTransactionCode() {
+        return transactionCode;
+    }
+
+    private TransactionType(int code) {
+        this.transactionCode = code;
+    }
 }
